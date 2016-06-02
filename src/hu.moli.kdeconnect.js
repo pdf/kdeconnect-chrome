2e93aac8d6be711cffaba30a22cc58c0b4f19346
Peter Fern <github@obfusc8.org>
2016-06-02 16:16:27 +1000

    Add context menu for sending links, etc.

--- src/hu.moli.kdeconnect.js
+++ src/hu.moli.kdeconnect.js
@@ -1,5 +1,15 @@
 chrome.browserAction.onClicked.addListener (function (tab) {
-  if (tab.url.indexOf ('chrome://') !== 0) {
-    chrome.runtime.connectNative ('hu.moli.kdeconnect').postMessage (tab.url);
+  if (tab.url.indexOf ("chrome://") !== 0) {
+    chrome.runtime.connectNative ("hu.moli.kdeconnect").postMessage (tab.url);
+  }
+});
+chrome.contextMenus.create({
+  "id": "kdeconnectUrlContext",
+  "title": "KDE Connect",
+  "contexts": ["page", "frame", "link", "image", "video", "audio"],
+});
+chrome.contextMenus.onClicked.addListener (function(info, tab) {
+  if (info.menuItemId === "kdeconnectUrlContext") {
+    chrome.runtime.connectNative ("hu.moli.kdeconnect").postMessage (info.linkUrl || info.srcUrl || info.frameUrl || info.pageUrl);
   }
 });
